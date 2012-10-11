8eeffbe04954b7ce15cf1d5186aa505e8f8da2b1
Mar Gro <gromar00@gmail.com>
2012-10-11 16:39:35 +0200

    SimpleProcessDiagramGeneratorTest fix

--- generator/src/test/java/org/processmonitor/generator/SimpleProcessDiagramGeneratorTest.java
+++ generator/src/test/java/org/processmonitor/generator/SimpleProcessDiagramGeneratorTest.java
@@ -1,11 +1,18 @@
 package org.processmonitor.generator;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RepositoryService;
@@ -24,7 +31,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class SimpleProcessDiagramGeneratorTest {
 
 	private static String FINANCIALREPORT_PROCESS_KEY = "financialReport";
-	private static String GENERATOR_PROCESS_KEY = "generateProcessDiagram";
+	private static String GENERATOR_PROCESS_KEY = "simpleprocessdiagramgeneratortest";
 
 	@Autowired 
 	private ProcessEngine processEngine;
@@ -37,7 +44,13 @@ public class SimpleProcessDiagramGeneratorTest {
 
 	@Before
 	public void before() {
-
+	  repositoryService.createDeployment()
+	    .addClasspathResource("org/activiti/examples/bpmn/usertask/FinancialReportProcess.bpmn20.xml")
+	    .addClasspathResource("org/activiti/examples/bpmn/usertask/FinancialReportProcess.png")
+	    .addClasspathResource("org/processmonitor/generator/SimpleProcessDiagramGenerator.bpmn")
+	    .addClasspathResource("org/processmonitor/generator/SimpleProcessDiagramGenerator.png")
+	    .addClasspathResource("org/processmonitor/generator/SimpleProcessDiagramGeneratorTest.bpmn")
+	    .deploy();
 	}
 
 	@After
@@ -62,11 +75,52 @@ public class SimpleProcessDiagramGeneratorTest {
 	    counts.put( "writeReportTask", 5);
 	    counts.put( "verifyReportTask", 25);
 	    params.put( "counts", counts);
-	    params.put( "reportFileName", "target/reportfile.png");
+	    params.put( "reportFileName", "target/SimpleProcessDiagramGeneratorTest.png");
 	    
 	    // start process
 		runtimeService.startProcessInstanceByKey(
 				GENERATOR_PROCESS_KEY, "GENERATOR-KEY-1", params).getId();
-	    
+	    //check outputs
+		InputStream expectedStream = new FileInputStream("src/test/resources/org/processmonitor/generator/SimpleProcessDiagramGeneratorTest.png" );   
+		InputStream generatedStream = new FileInputStream("target/SimpleProcessDiagramGeneratorTest.png");   
+	    assertTrue( isEqual(expectedStream, generatedStream));	    	
 	}
+	
+	static boolean isEqual(InputStream stream1, InputStream stream2)
+	          throws IOException {
+	  
+	      ReadableByteChannel channel1 = Channels.newChannel(stream1);
+	      ReadableByteChannel channel2 = Channels.newChannel(stream2);
+
+	      ByteBuffer buffer1 = ByteBuffer.allocateDirect(1024);
+	      ByteBuffer buffer2 = ByteBuffer.allocateDirect(1024);
+
+	      try {
+	          while (true) {
+
+	              int bytesReadFromStream1 = channel1.read(buffer1);
+	              int bytesReadFromStream2 = channel2.read(buffer2);
+
+	              if (bytesReadFromStream1 == -1 || bytesReadFromStream2 == -1) 
+	            	  return bytesReadFromStream1 == bytesReadFromStream2;
+
+	              buffer1.flip();
+	              buffer2.flip();
+
+	              for (int i = 0; i < Math.min(bytesReadFromStream1, bytesReadFromStream2); i++)
+	                  if (buffer1.get() != buffer2.get())
+	                      return false;
+
+	              buffer1.compact();
+	              buffer2.compact();
+	          }
+	      } catch (IOException e) {
+	    	  System.err.println( e.getStackTrace());
+	    	  throw e;
+	      } finally {
+	          if (stream1 != null) stream1.close();
+	          if (stream2 != null) stream2.close();
+	      }
+	  }
 }
+
