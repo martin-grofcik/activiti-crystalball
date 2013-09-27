8e7d3ed387a7a43c92d053a3407788c49c3c4ccb
gro-mar <gromar01@gmail.com>
2013-09-27 21:21:22 +0200

    Build Fix
    For unknown reason
    org.activiti.crystalball.diagram.HighlightNodeDiagramLayer remain
    unchanged and was not submitted to repository.

--- image-builder/src/main/java/org/activiti/crystalball/diagram/AbstractProcessDiagramLayerGenerator.java
+++ image-builder/src/main/java/org/activiti/crystalball/diagram/AbstractProcessDiagramLayerGenerator.java
@@ -9,9 +9,9 @@ package org.activiti.crystalball.diagram;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,13 +40,15 @@ public abstract class AbstractProcessDiagramLayerGenerator implements DiagramLay
     protected ProcessDiagramCanvasFactory canvasFactory;
 
 	public AbstractProcessDiagramLayerGenerator() {
-		super();
-        this.canvasFactory = new DefaultCanvasFactoryImpl();
+		this( null);
 	}
 
     public AbstractProcessDiagramLayerGenerator(ProcessDiagramCanvasFactory canvasFactory) {
         super();
-        this.canvasFactory = canvasFactory;
+        if ( canvasFactory != null)
+            this.canvasFactory = canvasFactory;
+        else
+            this.canvasFactory = new DefaultCanvasFactoryImpl();
     }
 
 	public RepositoryServiceImpl getRepositoryService() {
--- image-builder/src/main/java/org/activiti/crystalball/diagram/HighlightNodeDiagramLayer.java
+++ image-builder/src/main/java/org/activiti/crystalball/diagram/HighlightNodeDiagramLayer.java
@@ -9,9 +9,9 @@ package org.activiti.crystalball.diagram;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,21 +21,17 @@ package org.activiti.crystalball.diagram;
  */
 
 
-import java.awt.Color;
+import org.activiti.engine.impl.RepositoryServiceImpl;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
+
+import java.awt.*;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
-import org.activiti.engine.impl.RepositoryServiceImpl;
-import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
-import org.activiti.engine.impl.pvm.PvmTransition;
-import org.activiti.engine.impl.pvm.process.ActivityImpl;
-import org.activiti.engine.impl.pvm.process.Lane;
-import org.activiti.engine.impl.pvm.process.LaneSet;
-import org.activiti.engine.impl.pvm.process.ParticipantProcess;
-import org.activiti.engine.impl.pvm.process.TransitionImpl;
-
 /**
- * highlight nodes in the process diagram 
+ * highlight nodes in the process diagram
  *
  */
 public class HighlightNodeDiagramLayer extends AbstractProcessDiagramLayerGenerator implements DiagramLayerGenerator {
@@ -44,25 +40,30 @@ public class HighlightNodeDiagramLayer extends AbstractProcessDiagramLayerGenera
 	public static String HIGHLIGHTED_ACTIVITIES = "highLightedActivities";
 
 	/** color to highlight nodes */
-	protected Color color = Color.red;
-	
+	protected Color color;
+
 	public HighlightNodeDiagramLayer() {
 	}
-	
+
 	public HighlightNodeDiagramLayer( RepositoryServiceImpl repositoryService) {
-		this.repositoryService = repositoryService;
+		this(repositoryService, null, null);
 	}
 
-	public HighlightNodeDiagramLayer( RepositoryServiceImpl repositoryService, Color color) {
-		this( repositoryService);
-		this.color = color;
+	public HighlightNodeDiagramLayer( RepositoryServiceImpl repositoryService, Color color, ProcessDiagramCanvasFactory canvasFactory) {
+        super(canvasFactory);
+		this.repositoryService = repositoryService;
+        if (color != null)
+		    this.color = color;
+        else {
+            this.color = Color.red;
+        }
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	@Override
-	public byte[] generateLayer(String imageType, Map<String, Object> params) {
+	public InputStream generateLayer(String imageType, Map<String, Object> params) {
 
 		// get parameters
 		final String processDefinitionKey = (String) params.get( PROCESS_DEFINITION_ID );
@@ -72,7 +73,7 @@ public class HighlightNodeDiagramLayer extends AbstractProcessDiagramLayerGenera
 
 		// get process definition entity
 	    ProcessDefinitionEntity pde = (ProcessDefinitionEntity) ( ((RepositoryServiceImpl) repositoryService).getDeployedProcessDefinition( processDefinitionId ));
-	    
+
 		ProcessDiagramCanvas canvas = initProcessDiagramCanvas( pde );
 
 		for (ActivityImpl activity : pde.getActivities()) {
@@ -80,92 +81,9 @@ public class HighlightNodeDiagramLayer extends AbstractProcessDiagramLayerGenera
 	      if (highLightedActivities.contains(activity.getId())) {
 	    	  canvas.drawHighLight(activity.getX(), activity.getY(), activity.getWidth(), activity.getHeight(), color);	      }
 	    }
-	    return convertToByteArray( imageType, canvas.generateImage(imageType));
+	    return canvas.generateImage(imageType);
 	}
-
-	private ProcessDiagramCanvas initProcessDiagramCanvas(
-			ProcessDefinitionEntity processDefinition) {
-	    int minX = Integer.MAX_VALUE;
-	    int maxX = 0;
-	    int minY = Integer.MAX_VALUE;
-	    int maxY = 0;
-	    
-	    if(processDefinition.getParticipantProcess() != null) {
-	      ParticipantProcess pProc = processDefinition.getParticipantProcess();
-	      
-	      minX = pProc.getX();
-	      maxX = pProc.getX() + pProc.getWidth();
-	      minY = pProc.getY();
-	      maxY = pProc.getY() + pProc.getHeight();
-	    }
-	    
-	    for (ActivityImpl activity : processDefinition.getActivities()) {
-
-	      // width
-	      if (activity.getX() + activity.getWidth() > maxX) {
-	        maxX = activity.getX() + activity.getWidth();
-	      }
-	      if (activity.getX() < minX) {
-	        minX = activity.getX();
-	      }
-	      // height
-	      if (activity.getY() + activity.getHeight() > maxY) {
-	        maxY = activity.getY() + activity.getHeight();
-	      }
-	      if (activity.getY() < minY) {
-	        minY = activity.getY();
-	      }
-
-	      for (PvmTransition sequenceFlow : activity.getOutgoingTransitions()) {
-	        List<Integer> waypoints = ((TransitionImpl) sequenceFlow).getWaypoints();
-	        for (int i = 0; i < waypoints.size(); i += 2) {
-	          // width
-	          if (waypoints.get(i) > maxX) {
-	            maxX = waypoints.get(i);
-	          }
-	          if (waypoints.get(i) < minX) {
-	            minX = waypoints.get(i);
-	          }
-	          // height
-	          if (waypoints.get(i + 1) > maxY) {
-	            maxY = waypoints.get(i + 1);
-	          }
-	          if (waypoints.get(i + 1) < minY) {
-	            minY = waypoints.get(i + 1);
-	          }
-	        }
-	      }
-	    }
-	    
-	    if(processDefinition.getLaneSets() != null && processDefinition.getLaneSets().size() > 0) {
-	      for(LaneSet laneSet : processDefinition.getLaneSets()) {
-	        if(laneSet.getLanes() != null && laneSet.getLanes().size() > 0) {
-	          for(Lane lane : laneSet.getLanes()) {
-	            // width
-	            if (lane.getX() + lane.getWidth() > maxX) {
-	              maxX = lane.getX() + lane.getWidth();
-	            }
-	            if (lane.getX() < minX) {
-	              minX = lane.getX();
-	            }
-	            // height
-	            if (lane.getY() + lane.getHeight() > maxY) {
-	              maxY = lane.getY() + lane.getHeight();
-	            }
-	            if (lane.getY() < minY) {
-	              minY = lane.getY();
-	            }
-	          }
-	        }
-	      }
-	    }
-	    
-	    return new ProcessDiagramCanvas(maxX + 10, maxY + 10, minX, minY);
-	}
-
 	public void setColor(Color c) {
 		this.color = c;
 	}
-		
-
 }
